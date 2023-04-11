f4488d17847de28977807bb3ad5419d27a94276d
hi78s <hi78s@hanmail.net>
2023-04-11 14:36:10 +0900

    add rainbow

---
+++ hello5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	printf("hello world\n");
+	printf("hello world %d \n", __LINE__);
+	printf("hello world %d \n", __LINE__);
+	printf("hello world %d \n", __LINE__);
+}
